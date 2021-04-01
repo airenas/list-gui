a3117b11ff34633f7bd10226099b08ad521dfc40
airenas <airenass@gmail.com>
2021-04-01 17:16:39 +0300

    Fix ffmpeg loading

--- ear-app/src/app/service/mp4-extrator.service.ts
+++ ear-app/src/app/service/mp4-extrator.service.ts
@@ -2,6 +2,7 @@ import { FileUtils } from './../utils/file';
 import { Injectable } from '@angular/core';
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/internal/observable/of';
 
 @Injectable()
 export abstract class Mp4ExtratorService {
@@ -17,38 +18,52 @@ export class FFMpegMp4ExtratorService implements Mp4ExtratorService {
 
   extract(file: File): Observable<File> {
     return new Observable((observer) => {
-      const transcode = async (f) => {
-        try {
-          const ffmpeg = this.loadFFMpeg();
+      this.loadFFMpeg().subscribe(
+        {
+          next: (lib) => {
+            const transcode = async () => {
+              try {
+                lib.FS('writeFile', 'in.mp4', await fetchFile(file));
 
-          ffmpeg.FS('writeFile', 'in.mp4', await fetchFile(f));
+                console.log('Start copy audio');
+                await lib.run('-i', 'in.mp4', '-map', '0:a', '-acodec', 'copy', 'output.mp4');
 
-          console.log('Start copy audio');
-          await ffmpeg.run('-i', 'in.mp4', '-map', '0:a', '-acodec', 'copy', 'output.mp4');
+                console.log('Preparing file');
+                const data = lib.FS('readFile', 'output.mp4');
 
-          console.log('Preparing file');
-          const data = ffmpeg.FS('readFile', 'output.mp4');
-
-          observer.next(FileUtils.fromData(data, 'audio.mp4'));
-        } catch (e) {
-          observer.error(e);
-        }
-      };
-      transcode(file);
+                observer.next(FileUtils.fromData(data, 'audio.mp4'));
+              } catch (e) {
+                observer.error(e);
+              }
+              observer.complete();
+            };
+            transcode();
+          },
+          error: (e) => {
+            observer.error(e);
+          }
+        });
     });
   }
 
-  loadFFMpeg(): any {
-    if (this.ffmpegLib === undefined) {
+  loadFFMpeg(): Observable<any> {
+    if (this.ffmpegLib !== undefined) {
+      return of(this.ffmpegLib);
+    }
+    return new Observable((observer) => {
       const ffmpegTmp = createFFmpeg({
         log: false,
         // corePath: 'dist/ffmpeg-core.js'
       });
       console.log('Loading ffmpeg-core.js');
-      ffmpegTmp.load();
-      console.log('Loaded ffmpeg-core.js');
-      this.ffmpegLib = ffmpegTmp;
-    }
-    return this.ffmpegLib;
+      ffmpegTmp.load().then(() => {
+        console.log('Loaded ffmpeg-core.js');
+        this.ffmpegLib = ffmpegTmp;
+        observer.next(ffmpegTmp);
+      }).catch((err) => {
+        console.error('Error loading ffmpeg-core.js', err);
+        observer.error(err);
+      });
+    });
   }
 }
